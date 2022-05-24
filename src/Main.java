85ad0134cf5992c344211d5775c34a226799b58c
vrndarani <98180585+vrndarani@users.noreply.github.com>
2022-05-24 20:14:42 +0200

    hometask done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,64 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println(" Hello world!");
+        task1();
+        task2();
+        task3();
+
+
     }
-}
+
+    public static void task1 (){
+
+        int e = 0;
+        while (e < 10) {
+            System.out.print(++e + " ");
+        }
+        System.out.println();
+
+        for (int i = 10; i>0; i--){
+            System.out.print(i + " ");
+        }
+
+
+    }
+
+    public static void task2 () {
+        System.out.println("\n");
+
+      int i = 7;
+      while ( i <= 31){
+          System.out.println("Today is Friday " + i +" th. Report is due.");
+          i = i + 7;
+      }
+      }
+
+
+      public static void task3(){
+
+      int currentYear = 2022;
+      int start = currentYear - 200;
+      int end = currentYear + 100;
+
+      for (int year = start; year <= end; year++ ) {
+          if (year % 79 == 0){
+              System.out.println(year);
+          }
+      }
+
+
+
+      }
+
+
+
+
+      }
+
+
+
+
+
+
+
+
+
